10fa7db60ca445029bd5a69cf7656823fb595d1c
Kazadpour <kh.azadpour@gmail.com>
2025-10-17 13:13:04 -0700

    Implement code changes to enhance functionality and improve performance

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Windows;
 
@@ -15,5 +16,30 @@ public partial class App : Application
         {
             Directory.CreateDirectory(logDir);
         }
+
+        // Global exception handlers to diagnose startup/runtime crashes
+        this.DispatcherUnhandledException += (s, exArgs) =>
+        {
+            try
+            {
+                File.AppendAllText("C:/temp/VPOS_Reset_Log.log",
+                    $"[FATAL {DateTime.Now:yyyy-MM-dd HH:mm:ss}] Unhandled UI exception: {exArgs.Exception}\n");
+            }
+            catch { /* ignore logging failure */ }
+            MessageBox.Show($"An unexpected error occurred and was logged.\n{exArgs.Exception.Message}",
+                "Application Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            exArgs.Handled = true;
+        };
+
+        AppDomain.CurrentDomain.UnhandledException += (s, exArgs) =>
+        {
+            try
+            {
+                var ex = exArgs.ExceptionObject as Exception;
+                File.AppendAllText("C:/temp/VPOS_Reset_Log.log",
+                    $"[FATAL {DateTime.Now:yyyy-MM-dd HH:mm:ss}] Unhandled domain exception: {ex}\n");
+            }
+            catch { /* ignore logging failure */ }
+        };
     }
 }
